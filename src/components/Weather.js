4f9546e5ff2dbe081261bbeb426faa1b5f6f40f7
Poly Titova <poly_titova@mail.ru>
2021-01-23 14:58:08 +0300

    Добавили стили

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,14 +3,14 @@ import React from 'react';
 const Weather = props => (
   <div>
     { props.city &&
-      <div>
+      <div className="infoWeath">
         <p>Местоположение: {props.city}, {props.country}</p>
         <p>Температура: {props.temp}</p>
         <p>Давление: {props.pressure}</p>
         <p>Заход солнца: {props.sunset}</p>
       </div>
     }
-    <p>{props.error}</p>
+    <p className="error">{props.error}</p>
   </div>
 );
 
