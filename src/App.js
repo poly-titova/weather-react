d0f2bcf606e2ffb54d6312a5c23b56a8267f23b4
Poly Titova <poly_titova@mail.ru>
2021-01-23 11:15:56 +0300

    Сделали получение данных по url запросу

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Info from './components/Info';
 
+const API_KEY = '5e7f292d4d2e2431181edee119dd3a4c';
+
 class App extends React.Component {
+
+  gettingWeather = async () => {
+    const api_url = await 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+    console.log(data);
+  }
+
   render() {
     return (
       <div>
