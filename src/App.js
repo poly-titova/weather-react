89767355a3e69f562397e609280788255b21605f
Poly Titova <poly_titova@mail.ru>
2021-01-23 12:32:27 +0300

    Сделали вывод иформации погоды конкретного города

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import Info from './components/Info';
 import Form from './components/Form';
+import Weather from './components/Weather';
 
 const API_KEY = '5e7f292d4d2e2431181edee119dd3a4c';
 
 class App extends React.Component {
 
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
   gettingWeather = async (event) => {
     event.preventDefault();
     const city = event.target.elements.city.value;
-    const api_url = await 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-    const data = await api_url.json();
-    console.log(data);
+
+    if (city) {
+      const api_url = await
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const data = await api_url.json();
+      console.log(data);
+
+      this.setState({
+        temp: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        error: ""
+      });
+    }
   }
 
   render() {
@@ -20,6 +42,14 @@ class App extends React.Component {
       <div>
         <Info />
         <Form weatherMethod={this.gettingWeather} />
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          sunrise={this.state.sunrise}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div>
     );
   }
