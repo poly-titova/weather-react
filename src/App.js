bd9b63f79efaf39a91b8078a463453ca77e97a43
Poly Titova <poly_titova@mail.ru>
2021-01-23 11:40:47 +0300

    Сделали получение данных города, который введён в форму

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Info from './components/Info';
+import Form from './components/Form';
 
 const API_KEY = '5e7f292d4d2e2431181edee119dd3a4c';
 
 class App extends React.Component {
 
-  gettingWeather = async () => {
+  gettingWeather = async (event) => {
+    event.preventDefault();
+    const city = event.target.elements.city.value;
     const api_url = await 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await api_url.json();
     console.log(data);
   }
@@ -16,6 +19,7 @@ class App extends React.Component {
     return (
       <div>
         <Info />
+        <Form weatherMethod={this.gettingWeather} />
       </div>
     );
   }
