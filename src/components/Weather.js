acc38172e725606c8bc51906cbc3ea0efa37d4d8
Poly Titova <poly_titova@mail.ru>
2021-01-23 13:34:42 +0300

    Оптимизировали код компонентов

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,21 +1,17 @@
 import React from 'react';
 
-class Weather extends React.Component {
-  render() {
-    return (
+const Weather = props => (
+  <div>
+    { props.city &&
       <div>
-        { this.props.city &&
-          <div>
-            <p>Местоположение: {this.props.city}, {this.props.country}</p>
-            <p>Температура: {this.props.temp}</p>
-            <p>Давление: {this.props.pressure}</p>
-            <p>Заход солнца: {this.props.sunset}</p>
-          </div>
-        }
-        <p>{ this.props.error }</p>
+        <p>Местоположение: {props.city}, {props.country}</p>
+        <p>Температура: {props.temp}</p>
+        <p>Давление: {props.pressure}</p>
+        <p>Заход солнца: {props.sunset}</p>
       </div>
-    );
-  }
-}
+    }
+    <p>{props.error}</p>
+  </div>
+);
 
 export default Weather;
